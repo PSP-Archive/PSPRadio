1d086cc2cb2fdefb42403a67096cc04d14800757
raf <raf@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-09-24 01:37:31 +0000

    Removing old libnet

--- PSP/SharedLib/libnet/bits/endian.h
+++
@@ -1,8 +0,0 @@
-/* PSP is little-endian?.  */
-
-#ifndef _ENDIAN_H
-# error "Never use <bits/endian.h> directly; include <endian.h> instead."
-#endif
-
-//#define __BYTE_ORDER __BIG_ENDIAN
-#define __BYTE_ORDER __LITTLE_ENDIAN
