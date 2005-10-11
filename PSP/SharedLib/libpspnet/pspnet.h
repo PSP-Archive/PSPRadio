9e6f2861bcb4656b817f21bd09c9a171aefac07b
rafpsp <rafpsp@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-10-11 05:27:27 +0000

    Corrected an error in the disable network function, where sceNetTerm wasn't getting called. Also added credits to psppet

--- PSP/SharedLib/libpspnet/pspnet.h
+++ PSP/SharedLib/libpspnet/pspnet.h
@@ -1,4 +1,6 @@
-/* 
+/*
+ 	by PspPet
+	Modified by Raf with netbsd socket, etc.
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
