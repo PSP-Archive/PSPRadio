81720e6d8cf88e596a26c0e4d70b14da02385b49
rafpsp <rafpsp@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-10-11 16:08:32 +0000

    Forgot to check this in earlier

---
+++ PSP/PSPRadio/IPSPRadio_UI.cpp
@@ -0,0 +1,17 @@
+#include <PSPApp.h>
+#include <stdio.h>
+#include <unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <malloc.h>
+#include <iniparser.h>
+#include <Tools.h>
+#include <stdarg.h>
+#include "IPSPRadio_UI.h"
+
+IPSPRadio_UI::~IPSPRadio_UI()
+{
+	return;
+}
+
