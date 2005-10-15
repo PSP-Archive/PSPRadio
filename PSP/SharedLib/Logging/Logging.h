78f7d587a5b79e8ccf7d39c656ff84bee6ee1a2d
rafpsp <rafpsp@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-10-15 06:20:53 +0000

    Moved semaphore/lock/blocker classes from Logging.h to PSPApp

--- PSP/SharedLib/Logging/Logging.h
+++ PSP/SharedLib/Logging/Logging.h
@@ -17,77 +17,49 @@
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #ifndef _CLOGGINGH_
-#define _CLOGGINGH_
-
-#include <pspkernel.h>
-#include <pspkerneltypes.h>
-
-
-enum loglevel_enum
-{
-	LOG_VERYLOW  = 10,
-	LOG_LOWLEVEL = 20,
-	LOG_INFO	 = 50,
-	LOG_ERROR	 = 80,
-	LOG_ALWAYS	 = 100
-};
-
-class CLock
-{
-public:
-	CLock(char *strName){ m_mutex = sceKernelCreateSema(strName, 0, 1, 255, 0); }
-	~CLock() { sceKernelDeleteSema(m_mutex); }
+	#define _CLOGGINGH_
 	
-	int Lock() {	return sceKernelWaitSema(m_mutex, 1, NULL); };
-	int Unlock() { 	return sceKernelSignalSema(m_mutex, 1); }
-	int GetMutex() { return m_mutex; }
-private:
-	int m_mutex;
-	SceUInt m_timeout;
-};
-
-
-class CSema
-{
-public:
-	CSema(char *strName){ m_mutex = sceKernelCreateSema(strName, 0, 1, 255, 0); }
-	~CSema() { sceKernelDeleteSema(m_mutex); }
+	#include <pspkernel.h>
+	#include <pspkerneltypes.h>
+	#include <PSPSema.h>
 	
-	void Wait() {	sceKernelWaitSema(m_mutex, 1, NULL); };
-	void Up() { 	sceKernelSignalSema(m_mutex, 1); }
-	void Down() { 	sceKernelSignalSema(m_mutex, -1); }
-private:
-	int m_mutex;
-};
-
-class CLogging
-{
-public:
-	CLogging();
-	~CLogging();
-
-	int Set(char *strLogFilename, loglevel_enum iLogLevel);
-	void SetLevel(loglevel_enum iNewLevel);
 	
-	int Log_(char *strModuleName, int iLineNo, loglevel_enum LogLevel, char *strFormat, ...);
-	//int Log_(char *strModuleName, loglevel_enum LogLevel, char *strFormat, ...);
+	enum loglevel_enum
+	{
+		LOG_VERYLOW  = 10,
+		LOG_LOWLEVEL = 20,
+		LOG_INFO	 = 50,
+		LOG_ERROR	 = 80,
+		LOG_ALWAYS	 = 100
+	};
 	
-private:
-	char *m_strFilename;
-	loglevel_enum m_LogLevel;
-	FILE *m_fp;
-	CLock *m_lock;
-	char *m_msg;
-	clock_t m_timeInitial;
+	class CLogging
+	{
+	public:
+		CLogging();
+		~CLogging();
 	
-	/** fflush() doesn't work, so reopen for now */
-	void Open();
-	void Close();
+		int Set(char *strLogFilename, loglevel_enum iLogLevel);
+		void SetLevel(loglevel_enum iNewLevel);
+		
+		int Log_(char *strModuleName, int iLineNo, loglevel_enum LogLevel, char *strFormat, ...);
+		
+	private:
+		char *m_strFilename;
+		loglevel_enum m_LogLevel;
+		FILE *m_fp;
+		CLock *m_lock;
+		char *m_msg;
+		clock_t m_timeInitial;
+		
+		/** fflush() doesn't work, so reopen for now */
+		void Open();
+		void Close();
+		
+	};
 	
-};
-
-extern CLogging Logging;
-#define Log(level, format, args...) Logging.Log_(__FILE__, __LINE__, level, format, ## args)
-//#define Log(level, format, args...) Logging.Log_(__FILE__, level, format, ## args)
+	extern CLogging Logging;
+	#define Log(level, format, args...) Logging.Log_(__FILE__, __LINE__, level, format, ## args)
 
 #endif
+
