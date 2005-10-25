e4df92bc5e3312c020956169085d9f1eec95a37b
jpfouch48 <jpfouch48@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-10-25 13:06:17 +0000

    Cleaning up includes

--- PSP/PSPRadio/GraphicsUI/GraphicsUITheme.cpp
+++ PSP/PSPRadio/GraphicsUI/GraphicsUITheme.cpp
@@ -17,7 +17,7 @@
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "GraphicsUITheme.h"
-#include "Logging.h"
+//#include "Logging.h"
 //#include "Tools.h"
 
 
