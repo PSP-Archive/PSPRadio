6ce25da987acb05552c0b583770df3c7a8e3f28d
rafpsp <rafpsp@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2005-12-12 03:53:05 +0000

    Updated versions for next release

--- PSP/PSPRadio/PSPRadio.cpp
+++ PSP/PSPRadio/PSPRadio.cpp
@@ -64,7 +64,7 @@ private:
 	CScreenHandler *m_ScreenHandler;
 		
 public:
-	myPSPApp(): CPSPApp("PSPRadio", "0.37-pre7")
+	myPSPApp(): CPSPApp("PSPRadio", "0.37-pre8")
 	{
 		/** Initialize to some sensible defaults */
 		m_Config = NULL;
