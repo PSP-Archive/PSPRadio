fe1118f40fb737a21ceded4b811f4535a98b6f95
rafpsp <rafpsp@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2006-05-08 17:05:20 +0000

    Changed default thread stack size from (almost) 4KB to 8KB. Also, now threads delete themselves correctly when exiting. (in other words, leak corrected).

--- PSP/SharedLib/libpthread/pthread.c
+++ PSP/SharedLib/libpthread/pthread.c
@@ -19,7 +19,7 @@ int pthread_create(pthread_t *thread_id, const pthread_attr_t *attributes, threa
 	static int ctr = 0;
 	char strName[32];
 	int  initPriority = 0x20;
-	int  stackSize = 0xFA0;
+	int  stackSize = 8*1024; /* 8Kb stack */
 	SceUInt attr = PSP_THREAD_ATTR_USER;
 	SceKernelThreadOptParam *option = NULL;
 
@@ -42,12 +42,10 @@ int pthread_create(pthread_t *thread_id, const pthread_attr_t *attributes, threa
 /* pthreads terminate when they return, or if they call: */
 int pthread_exit(void *status)
 {
-	sceKernelExitThread(0);
+	sceKernelExitDeleteThread(0);
 	return 0;
 }
 
-///typedef void *(*thread_function)(void *)
-	
 int _pthread_lib_thread_handler(SceSize args, void *argp)
 {	
 	__pthread_lib_user_thread_and_args *ptdata = (__pthread_lib_user_thread_and_args *)argp;
@@ -56,28 +54,7 @@ int _pthread_lib_thread_handler(SceSize args, void *argp)
 	/** Call user thread */
 	(ptdata->tf)(ptdata->arguments);
 
-	sceKernelExitThread(0);
+	pthread_exit(NULL);
 
 	return iRet;
 }
-
-
-#if 0
-	
-		int Suspend()
-				{ return m_thid>=0?sceKernelSuspendThread(m_thid):-1; };
-		int Resume()
-				{ return m_thid>=0?sceKernelResumeThread(m_thid):-1; };
-		int WakeUp() /** Wakeup a thread that put itself to sleep with ThreadSleep() */
-				{ return m_thid>=0?sceKernelWakeupThread(m_thid):-1; };
-		int Wait(SceUInt *timeoutInUs) /** Wait until thread exits or timeout */
-				{ return m_thid>=0?sceKernelWaitThreadEnd(m_thid, timeoutInUs):-1; };
-		int WaitAndServiceCallbacks(SceUInt *timeoutInUs) /** Wait until thread exits(servicing callbacks) or timeout */
-				{ return m_thid>=0?sceKernelWaitThreadEndCB(m_thid, timeoutInUs):-1; };
-		int SetPriority(int iNewPriority)
-				{ return m_thid>=0?sceKernelChangeThreadPriority(m_thid, iNewPriority):-1; };
-
-	private:
-		int m_thid;
-	};
-#endif
