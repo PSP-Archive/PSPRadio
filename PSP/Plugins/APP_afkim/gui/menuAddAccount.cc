63bbf7868a221346551f7bbde36dd1a0e50eadb2
danzel <danzel@ee41d4da-33fd-0310-aed6-9b5a9ddb5d53>
2007-03-11 09:08:31 +0000

    Add files that svn wouldn't let me commit. These are what was previously menuPopup.*

---
+++ PSP/Plugins/APP_afkim/gui/menuAddAccount.cc
@@ -0,0 +1,74 @@
+#include "menuAddAccount.h"
+#include "../dlib/util.h"
+#include "../bitlbee.h"
+#include "accountCreator.h"
+
+menuAddAccount::menuAddAccount()
+{
+	init(20, 172, 12, 0, "./pics/selected.png"); 
+	bgimage = new guiBit("./pics/menu.png", false);
+	bgimage->moveTo(1,146);
+}
+
+menuAddAccount::~menuAddAccount()
+{
+	delete bgimage;
+}
+
+void menuAddAccount::draw()
+{
+	dirty = false;
+	if (!gui_active)
+		return;
+	bgimage->draw();
+	selector::draw();
+}
+
+string menuAddAccount::getInputKey() const
+{
+	return "menuAddAccount";
+}
+
+//functions called when things happen in takeInput,
+//the value returned here is the value returned to takeInput
+string menuAddAccount::fixSelected()
+{
+	if (selected < 0) selected = 5;
+	if (selected > 5) selected = 0;
+	return getInputKey();
+}
+
+string menuAddAccount::pressCross()
+{
+	if (selected == 5)
+		return SWITCH_QUIT;
+
+	accountCreator* newAC = NULL;
+#warning menuAddAccount "menuAddAccount" in accountCreator = correct behaviour?
+	switch(selected)
+	{
+	case 0:
+		newAC = new accountCreator(BAT_AOL, unicodeClean("Enter details for your AIM Account"), "menuAddAccount");
+		break;
+	case 1:
+		newAC = new accountCreator(BAT_ICQ, unicodeClean("Enter details for your ICQ Account\nYour username is your ICQ number"), "menuAddAccount");
+		break;
+	case 2:
+		newAC = new accountCreator(BAT_MSN, unicodeClean("Enter details for your MSN Account\nFull email address, like: someone@msn.com"), "menuAddAccount");
+		break;
+	case 3:
+		newAC = new accountCreator(BAT_GMAIL, unicodeClean("Enter details for your GTalk Account\nYou do not need to add '@gmail.com'.\nIt is done automatically."), "menuAddAccount");
+		break;
+	case 4:
+		newAC = new accountCreator(BAT_YAHOO, unicodeClean("Enter details for your Yahoo Account"), "menuAddAccount");
+		break;
+	}
+	inputs["accountCreator"] = newAC;
+	return "accountCreator";
+}
+
+string menuAddAccount::pressSelect()
+{
+	holdingOne = true;
+	return "menuMain";
+}
---
+++ PSP/Plugins/APP_afkim/gui/menuAddAccount.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDED_GUI_MENUADDACCOUNT_H
+#define INCLUDED_GUI_MENUADDACCOUNT_H
+
+#include "../dlib/guibits/selector.h"
+
+class menuAddAccount : public selector
+{
+public:
+	menuAddAccount();
+	~menuAddAccount();
+	
+	//From guibit:
+	void draw();
+	
+	//From inputable:
+	string getInputKey() const;
+protected:
+	//functions called when things happen in takeInput,
+	//the value returned here is the value returned to takeInput
+	virtual string fixSelected();
+	virtual string pressCross();
+	virtual string pressSelect();
+private:
+	guiBit* bgimage;
+};
+
+#endif
